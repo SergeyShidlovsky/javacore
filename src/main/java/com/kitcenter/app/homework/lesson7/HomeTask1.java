6f99e09ac3e841220d2205d6edd40980c37c276f
Sergey Shidlovsky <shedlovskys@gmail.com>
2017-09-24 21:46:16 +0300

    first task of Home task 7 have been comleted

---
+++ src/main/java/com/kitcenter/app/homework/lesson7/HomeTask1.java
@@ -0,0 +1,47 @@
+package com.kitcenter.app.homework.lesson7;
+
+import java.util.Arrays;
+
+/**
+ * Created by Nastya on 24.09.2017.
+ */
+public class Task1 {
+
+    //Initialize massive
+    int[] arr = new int[10];
+
+    //Method for an array creation
+    public void createArray(){
+
+        //Initialize first number of massive
+        int number = 0;
+        //Filling array by numbers
+        for (int i = 0; i <= (arr.length -1); i++){
+            arr[i] = number;
+            number = number + 2;
+        }
+    }
+
+    //Method that shows array in string
+    public void showArrayString(){
+        System.out.println("There are 2 way for showing array in one string" +
+                            "\n first is arrayToString() method");
+        System.out.println(Arrays.toString(arr));
+        System.out.println("The second way is to print each elemen of massive after each loop iiteration");
+
+        System.out.println("[ ");
+        for (int i = 0 ; i <= (arr.length - 1); i++ ){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("]");
+    }
+
+    //Method that shows arrya in colonum
+    public void showArrayColonum(){
+        System.out.println("Let's print in colonum each element of massive after each loop iteration");
+
+        for (int i = 0 ; i <= (arr.length - 1); i++ ){
+            System.out.println(arr[i]);
+        }
+    }
+}
