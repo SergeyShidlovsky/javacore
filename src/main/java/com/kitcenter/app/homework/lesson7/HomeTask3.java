f5ed193c8aa57be38ec2ca20b6300e6b632a07ef
Sergey Shidlovsky <shedlovskys@gmail.com>
2017-09-26 01:00:21 +0300

    completed home task 3

---
+++ src/main/java/com/kitcenter/app/homework/lesson7/HomeTask3.java
@@ -0,0 +1,7 @@
+package com.kitcenter.app.homework.lesson7;
+
+/**
+ * Created by Nastya on 25.09.2017.
+ */
+public class HomeTask3 {
+}
---
+++ src/main/java/com/kitcenter/runners/homework/lesson7/HomeTask3Running.java
@@ -0,0 +1,7 @@
+package com.kitcenter.runners.homework.lesson7;
+
+/**
+ * Created by Nastya on 25.09.2017.
+ */
+public class HomeTask3Running {
+}
