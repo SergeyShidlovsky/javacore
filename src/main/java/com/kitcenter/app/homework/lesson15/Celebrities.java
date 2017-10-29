eed5984f6a4fdbafea34ce21b8e62a9ab72a3be4
Sergey Shidlovsky <shedlovskys@gmail.com>
2017-10-29 20:50:21 +0200

    Completed all Home taks 15

--- src/main/java/com/kitcenter/app/homework/lesson15/Celebrities.java
+++ src/main/java/com/kitcenter/app/homework/lesson15/Celebrities.java
@@ -3,18 +3,18 @@ package com.kitcenter.app.homework.lesson15;
 /**
  * Created by Nastya on 28.10.2017.
  */
-public enum Selebrities {
-    CEZAR("Cezar",42, "dead"), CALLIGULA("Calligula", 32, "dead"), NEYRON("Neyron", 39, "dead" ),
-    GANNIBAL("Gannibal", 62, "dead"), GENGHIS("Genghis", 43, "dead"), ALEXANDER("Alexander", 35, "dead"),
-    ARISTO("Aristo",45, "dead"), FRANKLYN("Franklyn", 65, "dead"), ROUZVELT("Rouzvelt", 62, "dead"),
-    CLINTON("Clinton", 67, "alive");
+public enum Celebrities {
+    CEZAR("Cezar","42", "dead"), CALLIGULA("Calligula", "32", "dead"), NEYRON("Neyron", "39", "dead" ),
+    GANNIBAL("Gannibal", "62", "dead"), GENGHIS("Genghis", "43", "dead"), ALEXANDER("Alexander", "35", "dead"),
+    ARISTO("Aristo","45", "dead"), FRANKLYN("Franklyn", "65", "dead"), ROUZVELT("Rouzvelt", "62", "dead"),
+    CLINTON("Clinton", "67", "alive");
 
     //Each of enumerations will have next fields
     private String name;
-    private int age;
+    private String age;
     private String status;
 
-    Selebrities(String name, int age, String status) {
+    Celebrities(String name, String age, String status) {
         this.name = name;
         this.age = age;
         this.status = status;
@@ -26,7 +26,7 @@ public enum Selebrities {
     }
 
     //Method that returns Age of one of enumerations
-    public int getAge() {
+    public String getAge() {
         return age;
     }
 
--- src/main/java/com/kitcenter/runners/homework/lesson15/CelebritiesRunner.java
+++ src/main/java/com/kitcenter/runners/homework/lesson15/CelebritiesRunner.java
@@ -1,31 +1,84 @@
 package com.kitcenter.runners.homework.lesson15;
 
-import com.kitcenter.app.homework.lesson15.Selebrities;
+import com.kitcenter.app.homework.lesson15.Celebrities;
 
 import java.util.Scanner;
 
 /**
  * Created by Nastya on 28.10.2017.
  */
-public class SelebritiesRunner {
+public class CelebritiesRunner {
 
     public static void main(String[] args) {
 
-        //Exporting all Selebrities objects to one array "selebrities[]"
-        Selebrities selebrities[] = Selebrities.values();
+        //Launching CelebritiesRunner
+        runCelebritiesRunner();
 
     }
 
     //Method with main running logic
-    public static void runSelebritiesRunner( Selebrities selebrities[] ){
+    public static void runCelebritiesRunner( ){
 
-        //Randomly selecting object Selebrity
-        String selebrities[(int) (Math.random()*10)].getName();
+        //Exporting all Celebrities objects to one array "celebrities[]"
+        Celebrities celebrities[] = Celebrities.values();
+
+        //Public "WinCounter" variable to stop program running after 3 win of game
+        int winCounter = 0;
+
+        //Game will be closed after 3 win of game
+        do {
+           winCounter = executeScanningComparing(celebrities, winCounter);
+            System.out.println("Counter" + winCounter);
+            //Checking if user has 3 Successful try of game
+            if (winCounter == 3) {
+                System.out.println("YOU WIN. Game is over");
+            }
+            else {
+                System.out.println("Press \"C\" to Continue game || Press \"Q\" to Exit game ");
+                String reset = scanning();
+                if (reset.toLowerCase().equals("q")){
+                    System.out.println("Thanks for attention");
+                }
+            }
+        } while (winCounter < 3);
     }
 
-    //Method that is comparing user input data and info from "Selebrities" Enumeration
-    public static void compareSelebrity(String age, String status){
+    //Method that executes main logic of running several times
+    public  static int executeScanningComparing(Celebrities celebrities[], int winCounter){
+        //Randomly selecting object Celebrity
+        Celebrities randomSelectedCelebrity = selectCelebrity(celebrities);
+        String requestName = randomSelectedCelebrity.getName();
 
+        System.out.print("Please, input Age of this Person: " + requestName + " :");
+        //Scanning Age of Celebrity
+        String requestAge = scanning();
+        System.out.print("Please, input  Status (Dead / Alive) of Person : " + requestName + " :");
+        //Scanning requestDead/Alive Status Celebrity
+        String requestStatus = scanning();
+
+        //Compaiting input info with info from Enumeration
+        winCounter = compareSelebrity(requestAge,requestStatus,randomSelectedCelebrity, winCounter);
+
+        return winCounter;
+    }
+
+    //Method that Randomly selects object from Enumeration Celebrity
+    public static Celebrities selectCelebrity( Celebrities celebrities[]){
+        Celebrities selectedCelebrity = celebrities[(int) (Math.random()*10)];
+        return selectedCelebrity;
+    }
+
+    //Method that is comparing user input data and info from "Celebrities" Enumeration
+    public static int compareSelebrity(String requestAge, String requestStatus,
+                                        Celebrities selectedCelebrity,int winCounter){
+        if (( (requestAge.toLowerCase()).equals(selectedCelebrity.getAge().toLowerCase())) &&
+                (requestStatus.toLowerCase().equals(selectedCelebrity.getStatus().toLowerCase()))) {
+            System.out.println("Successful try");
+            winCounter++;
+        } else {
+            System.out.println("UnSuccessful try");
+        }
+        return winCounter;
     }
 
     //Method that is scanning data from PC
